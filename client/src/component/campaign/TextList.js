636898bd6c7eb37bbd81debc5093a618c750141f
jisoooo17 <jisoooo17@gmail.com>
2024-02-22 14:51:52 +0900

    캠페인 목록페이지 썸네일 노출되도록 코드 수정

--- client/src/component/campaign/TextList.js
+++ client/src/component/campaign/TextList.js
@@ -1,12 +1,42 @@
 import { useNavigate } from 'react-router-dom';
-// import bg from '../../../public/img/img-campaign.png';
+
 
 const TextList = ({campaignList}) => {
   const navigate = useNavigate();
+
+
+  // 이미지 태그의 src 속성값을 추출하는 함수
+  const extractImageSrc = (htmlString) => {
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = htmlString;
+    const imgTags = tempElement.getElementsByTagName('img');
+    const srcList = [];
+    for (let i = 0; i < imgTags.length; i++) {
+      srcList.push(imgTags[i].getAttribute('src'));
+    }
+    return srcList;
+  };
+
+  // campaignList.body 내부의 이미지 src 추출
+  const imageSrcList = extractImageSrc(campaignList.body);
+
   return (
     <button className="cont" onClick={()=>{navigate(`/campaign/detail/${campaignList.id}`)}}>
-      <div className="img-wrap img-resize-w">
+      {/* <div className="img-wrap img-resize-w">
+        {console.log(campaignList.body)}
+      </div> */}
+      {/* <div className="img-wrap img-resize-w">
         <img src={process.env.PUBLIC_URL + '/img/img-campaign-def.jpg'}/>
+      </div> */}
+
+      <div className="img-wrap img-resize-w">
+        {imageSrcList.length > 0 ? (
+          imageSrcList.map((src, index) => (
+            <img key={index} src={src} alt={`Image ${index}`} />
+          ))
+        ) : (
+          <img src={process.env.PUBLIC_URL + '/img/img-campaign-def.jpg'} alt="Default Campaign Image" />
+        )}
       </div>
 
       <div className="txt-wrap">
