767e9f5c077e66e6f7de168ca4e5586e148ca990
jisoooo17 <jisoooo17@gmail.com>
2024-03-04 12:03:39 +0900

    캠페인 - 마이페이지 작업중

--- client/src/views/Mypage.js
+++ client/src/views/Mypage.js
@@ -3,22 +3,22 @@ import { getPost } from '../store/store';
 import { useDispatch } from 'react-redux';
 import Header from "../component/Header";
 import Footer from "../component/Footer";
+
 function Mypage() {
   const dispatch = useDispatch();
   const storedUserData = sessionStorage.getItem("userData");
   const userData = JSON.parse(storedUserData);
 
-
   // 글 목록
-  const [campaignList, setCampaignList] = useState([]); // 캠페인 목록을 저장 
+  const [mypostList, setMypostList] = useState([]); // 캠페인 목록을 저장 
+
   // 캠페인 데이터 불러옴
   useEffect(() => {
-    // 포스트 데이터를 불러와서 campaignList state에 저장
     dispatch(getPost())
       .then((res) => {
         if (res.payload) {
           let arrPost = [...res.payload];
-          setCampaignList(arrPost.reverse());
+          setMypostList(arrPost.reverse());
         }
       })
       .catch((err) => console.log(err));
@@ -26,6 +26,15 @@ function Mypage() {
 
   return (
     <div id="wrap">
+      {/* 내가 쓴 캠페인 글 */}
+      <div className="mypost">
+        <ul>
+          {mypostList.filter(item => item.userid === userData.userid).map((post, index) => (
+            <li key={index}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+
       <Header />
       <div className="content-w">
         <div className="inner">
