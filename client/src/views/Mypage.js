a5d67fd68c8520a81df3c9bfba6302d4634e101f
jisoooo17 <jisoooo17@gmail.com>
2024-03-04 11:44:15 +0900

    마이페이지 캠페인 영역 작업 시작

--- client/src/views/Mypage.js
+++ client/src/views/Mypage.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { getPost } from '../store/store';
+import { useDispatch } from 'react-redux';
 import Header from "../component/Header";
 import Footer from "../component/Footer";
 function Mypage() {
+  const dispatch = useDispatch();
+  const storedUserData = sessionStorage.getItem("userData");
+  const userData = JSON.parse(storedUserData);
+
+
+  // 글 목록
+  const [campaignList, setCampaignList] = useState([]); // 캠페인 목록을 저장 
+  // 캠페인 데이터 불러옴
+  useEffect(() => {
+    // 포스트 데이터를 불러와서 campaignList state에 저장
+    dispatch(getPost())
+      .then((res) => {
+        if (res.payload) {
+          let arrPost = [...res.payload];
+          setCampaignList(arrPost.reverse());
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div id="wrap">
       <Header />
