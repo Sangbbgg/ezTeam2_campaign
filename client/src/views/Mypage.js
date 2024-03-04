bc6b1f8435e68490d7e269abe17db0de638dbf9a
jisoooo17 <jisoooo17@gmail.com>
2024-03-04 13:48:58 +0900

    마이페이지 내가 쓴 글 영역 수정중

--- client/src/views/Mypage.js
+++ client/src/views/Mypage.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { getPost } from '../store/store';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { getPost } from '../store/store';
 import Header from "../component/Header";
 import Footer from "../component/Footer";
 
 function Mypage() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const storedUserData = sessionStorage.getItem("userData");
   const userData = JSON.parse(storedUserData);
 
@@ -35,76 +37,75 @@ function Mypage() {
           </div>
 
             {/* 내가 쓴 캠페인 글 */}
-            <div className="mycont-w">
+            <div className="mycont-wrap">
             <h3 className="title">내가 쓴 글</h3>
-              <div>
+              <div className="mypost-w">
                 {mypostList.filter(item => item.userid === userData.userid).map((post, index) => (
-                  <div className="post" key={index}>
-                    <div className="summary-wrap">
-                      <div className="title-area">
-                        <p className="title">{post.title}</p>
-                        <div className="regi-info">
-                        
-                        <p className="views">{"조회수: " + parseInt(post.views + 1)}</p>
-                        <p className="date">{new Date(post.date).toLocaleDateString()}</p>
-                        </div>
-                      </div>
-
-                      <div className="info-area">
-                        <div className="detail-info">
-                          {/* 캠페인 기간 */}
-                          {
-                            post.end_date != null ? (
-                              <div className="info-box">
-                                <p className='tit'>캠페인 기간</p>
-                              
-                                <div className="date-wrap">
-                                  {post.start_date != null ? (
-                                    <p className="start-date">{new Date(post.start_date).toLocaleDateString()}</p>
-                                  ) : null}
-                                    <span>~</span>
-                                  {post.end_date != null ? (
-                                    <p className="end-date">{new Date(post.end_date).toLocaleDateString()}</p>
-                                  ) : null}
-                                </div>
-                              </div>
-                            ): null
-                          }
-
-                          {/* 접수 기간 */}
-                          {
-                            post.end_date != null ? (
-                              <div className="info-box">
-                                <p className='tit'>접수 기간</p>
-                              
-                                <div className="date-wrap">
-                                  {post.reception_start_date != null ? (
-                                    <p className="start-date">{new Date(post.reception_start_date).toLocaleDateString()}</p>
-                                  ) : null}
-                                    <span>~</span>
-                                  {post.reception_end_date != null ? (
-                                    <p className="end-date">{new Date(post.reception_end_date).toLocaleDateString()}</p>
-                                  ) : null}
-                                </div>
-                              </div>
-                            ): null
-                          }
-
-                          {/* 위치 정보 */}
-                          {
-                            post.latitude != null ? (
-                              <div className="info-box">
-                                <p className="tit">캠페인 장소</p>
-                                  <div className="txt-w">
-                                    <p className="txt">{post.address}</p>
-                                    <p className="detail-txt">{post.address_detail}</p>
-                                  </div>
-                              </div>
-                            ):null
-                          }
-                        </div>
+                  <div className="mypost summary-wrap" key={index}>
+                    <div className="title-area">
+                      <p className="title">{post.title}</p>
+                      <div className="regi-info">
+                      
+                      {/* <p className="views">{"조회수: " + parseInt(post.views + 1)}</p> */}
+                      <p className="date">{new Date(post.date).toLocaleDateString()}</p>
                       </div>
                     </div>
+
+                    <div className="info-area">
+                      <div className="detail-info">
+                        {/* 캠페인 기간 */}
+                        {
+                          post.end_date != null ? (
+                            <div className="info-box">
+                              <p className='tit'>캠페인 기간</p>
+                            
+                              <div className="date-wrap">
+                                {post.start_date != null ? (
+                                  <p className="start-date">{new Date(post.start_date).toLocaleDateString()}</p>
+                                ) : null}
+                                  <span>~</span>
+                                {post.end_date != null ? (
+                                  <p className="end-date">{new Date(post.end_date).toLocaleDateString()}</p>
+                                ) : null}
+                              </div>
+                            </div>
+                          ): null
+                        }
+
+                        {/* 접수 기간 */}
+                        {
+                          post.end_date != null ? (
+                            <div className="info-box">
+                              <p className='tit'>접수 기간</p>
+                            
+                              <div className="date-wrap">
+                                {post.reception_start_date != null ? (
+                                  <p className="start-date">{new Date(post.reception_start_date).toLocaleDateString()}</p>
+                                ) : null}
+                                  <span>~</span>
+                                {post.reception_end_date != null ? (
+                                  <p className="end-date">{new Date(post.reception_end_date).toLocaleDateString()}</p>
+                                ) : null}
+                              </div>
+                            </div>
+                          ): null
+                        }
+
+                        {/* 위치 정보 */}
+                        {
+                          post.latitude != null ? (
+                            <div className="info-box">
+                              <p className="tit">캠페인 장소</p>
+                                <div className="txt-w">
+                                  <p className="txt">{post.address}</p>
+                                  <p className="detail-txt">{post.address_detail}</p>
+                                </div>
+                            </div>
+                          ):null
+                        }
+                      </div>
+                    </div>
+                    <button className='btn-view' onClick={()=>{navigate(`/campaign/detail/${post.id}`)}} >보러가기</button>
                   </div>
                 ))}
               </div>
