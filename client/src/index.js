818de0d29f2ea70cdb461652c8c7ed646f18ef49
jisoooo17 <jisoooo17@gmail.com>
2024-03-05 14:01:55 +0900

    페이지 이동 시 자동으로 스크롤 맨 위로 오게 하는 ScrollTop 컴포넌트 추가

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import ScrollTop from './component/ScrollTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
+      <ScrollTop/>
       <App />
     </BrowserRouter>
   </Provider>
