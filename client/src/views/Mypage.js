88d289a1a36fc080870376100f47e01dc49e7d70
Sangbbgg <151603886+Sangbbgg@users.noreply.github.com>
2024-03-06 15:11:53 +0900

    마이페이지발자국 차트 작성중

--- client/src/views/Mypage.js
+++ client/src/views/Mypage.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { getPost } from '../store/store';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getPost } from "../store/store";
+import axios from "axios";
 import Header from "../component/Header";
 import Footer from "../component/Footer";
 // user 월별 차트
-import UserMonthChart from "../component/CarbonFootprints/Result/userMonthChart"
+import UserMonthChart from "../component/CarbonFootprints/Result/UserMonthChart";
 
 function Mypage() {
   const dispatch = useDispatch();
@@ -16,12 +16,12 @@ function Mypage() {
   const userData = JSON.parse(storedUserData);
 
   // 글 목록
-  const [campaignList, setCampaignList] = useState([]); // 내가 쓴 글 
-  const [myApplication, setMyApplication] = useState([]); // 신청한 캠페인 목록 
+  const [campaignList, setCampaignList] = useState([]); // 내가 쓴 글
+  const [myApplication, setMyApplication] = useState([]); // 신청한 캠페인 목록
 
   // 현재 선택된 탭 인덱스
   const [activeTab, setActiveTab] = useState(0);
-  
+
   // 캠페인 데이터 불러옴
   useEffect(() => {
     dispatch(getPost())
@@ -39,14 +39,13 @@ function Mypage() {
     const fetchUserInfo = async () => {
       try {
         const response = await axios.get(`http://localhost:8000/campaign/form/all`);
-        const filteredData =  response.data.filter((item)=>{
-          return item.userid === userData.userid
-          
-        })
-    
+        const filteredData = response.data.filter((item) => {
+          return item.userid === userData.userid;
+        });
+
         setMyApplication(filteredData);
       } catch (error) {
-        console.error('Error fetching user info:', error);
+        console.error("Error fetching user info:", error);
       }
     };
 
@@ -66,7 +65,7 @@ function Mypage() {
       }
     });
   };
-  
+
   return (
     <div id="wrap" className="mypage">
       <Header />
@@ -79,19 +78,27 @@ function Mypage() {
             {/* 탭 버튼 영역 */}
             <div className="tab-area">
               {/* 각 탭 클릭시 handleTabClick 함수 호출 */}
-              <button className={`btn-tab ${activeTab === 0 ? 'active' : ''}`} onClick={() => handleTabClick(0)}>내가 쓴 글</button>
-              <button className={`btn-tab ${activeTab === 1 ? 'active' : ''}`} onClick={() => handleTabClick(1)}>신청한 캠페인</button>
-              <button className={`btn-tab ${activeTab === 2 ? 'active' : ''}`} onClick={() => handleTabClick(2)}>탄소 발자국</button>
+              <button className={`btn-tab ${activeTab === 0 ? "active" : ""}`} onClick={() => handleTabClick(0)}>
+                내가 쓴 글
+              </button>
+              <button className={`btn-tab ${activeTab === 1 ? "active" : ""}`} onClick={() => handleTabClick(1)}>
+                신청한 캠페인
+              </button>
+              <button className={`btn-tab ${activeTab === 2 ? "active" : ""}`} onClick={() => handleTabClick(2)}>
+                탄소 발자국
+              </button>
             </div>
             {/* 콘텐츠 영역 */}
             <div className="content-area">
               {/* 내가 쓴 캠페인 글 */}
               {activeTab === 0 && (
                 <div className="mycont-wrap">
-                    <div className="cont-area">
-                      <div className="post-list">        
-                        {campaignList.filter(item => item.userid === userData.userid).length > 0 ? (
-                          campaignList.filter(item => item.userid === userData.userid).map((post, index) => (
+                  <div className="cont-area">
+                    <div className="post-list">
+                      {campaignList.filter((item) => item.userid === userData.userid).length > 0 ? (
+                        campaignList
+                          .filter((item) => item.userid === userData.userid)
+                          .map((post, index) => (
                             <div className="mypost summary-wrap" key={index}>
                               <div className="title-area">
                                 <p className="title">{post.title}</p>
@@ -107,13 +114,9 @@ function Mypage() {
                                     <div className="info-box">
                                       <p className="tit">캠페인 기간</p>
                                       <div className="date-wrap">
-                                        {post.start_date && (
-                                          <p className="start-date">{new Date(post.start_date).toLocaleDateString()}</p>
-                                        )}
+                                        {post.start_date && <p className="start-date">{new Date(post.start_date).toLocaleDateString()}</p>}
                                         <span>~</span>
-                                        {post.end_date && (
-                                          <p className="end-date">{new Date(post.end_date).toLocaleDateString()}</p>
-                                        )}
+                                        {post.end_date && <p className="end-date">{new Date(post.end_date).toLocaleDateString()}</p>}
                                       </div>
                                     </div>
                                   )}
@@ -122,13 +125,9 @@ function Mypage() {
                                     <div className="info-box">
                                       <p className="tit">접수 기간</p>
                                       <div className="date-wrap">
-                                        {post.reception_start_date && (
-                                          <p className="start-date">{new Date(post.reception_start_date).toLocaleDateString()}</p>
-                                        )}
+                                        {post.reception_start_date && <p className="start-date">{new Date(post.reception_start_date).toLocaleDateString()}</p>}
                                         <span>~</span>
-                                        {post.reception_end_date && (
-                                          <p className="end-date">{new Date(post.reception_end_date).toLocaleDateString()}</p>
-                                        )}
+                                        {post.reception_end_date && <p className="end-date">{new Date(post.reception_end_date).toLocaleDateString()}</p>}
                                       </div>
                                     </div>
                                   )}
@@ -145,19 +144,26 @@ function Mypage() {
                                 </div>
                               </div>
                               <div className="btn-w">
-                                <button className="btn-view" onClick={() => {navigate(`/campaign/detail/${post.id}`)}}>보러가기</button>
+                                <button
+                                  className="btn-view"
+                                  onClick={() => {
+                                    navigate(`/campaign/detail/${post.id}`);
+                                  }}
+                                >
+                                  보러가기
+                                </button>
                               </div>
                             </div>
                           ))
-                        ) : (
-                          <div className="no-data-w">
-                            <div className="no-data">
-                              <p className="tit">내가 쓴 글이 없습니다.</p>
-                            </div>
+                      ) : (
+                        <div className="no-data-w">
+                          <div className="no-data">
+                            <p className="tit">내가 쓴 글이 없습니다.</p>
                           </div>
-                        )}
-                      </div>
+                        </div>
+                      )}
                     </div>
+                  </div>
                 </div>
               )}
               {/* 신청한 캠페인 */}
@@ -167,7 +173,7 @@ function Mypage() {
                     <div className="post-list">
                       {myApplication.length > 0 ? (
                         myApplication.map((application, index) => {
-                          const matchedPost = campaignList.find(post => post.id === application.post_id);
+                          const matchedPost = campaignList.find((post) => post.id === application.post_id);
                           if (matchedPost) {
                             return (
                               <div className="mypost summary-wrap" key={index}>
@@ -184,13 +190,9 @@ function Mypage() {
                                       <div className="info-box">
                                         <p className="tit">캠페인 기간</p>
                                         <div className="date-wrap">
-                                          {matchedPost.start_date && (
-                                            <p className="start-date">{new Date(matchedPost.start_date).toLocaleDateString()}</p>
-                                          )}
+                                          {matchedPost.start_date && <p className="start-date">{new Date(matchedPost.start_date).toLocaleDateString()}</p>}
                                           <span>~</span>
-                                          {matchedPost.end_date && (
-                                            <p className="end-date">{new Date(matchedPost.end_date).toLocaleDateString()}</p>
-                                          )}
+                                          {matchedPost.end_date && <p className="end-date">{new Date(matchedPost.end_date).toLocaleDateString()}</p>}
                                         </div>
                                       </div>
                                     )}
@@ -199,13 +201,9 @@ function Mypage() {
                                       <div className="info-box">
                                         <p className="tit">접수 기간</p>
                                         <div className="date-wrap">
-                                          {matchedPost.reception_start_date && (
-                                            <p className="start-date">{new Date(matchedPost.reception_start_date).toLocaleDateString()}</p>
-                                          )}
+                                          {matchedPost.reception_start_date && <p className="start-date">{new Date(matchedPost.reception_start_date).toLocaleDateString()}</p>}
                                           <span>~</span>
-                                          {matchedPost.reception_end_date && (
-                                            <p className="end-date">{new Date(matchedPost.reception_end_date).toLocaleDateString()}</p>
-                                          )}
+                                          {matchedPost.reception_end_date && <p className="end-date">{new Date(matchedPost.reception_end_date).toLocaleDateString()}</p>}
                                         </div>
                                       </div>
                                     )}
@@ -222,8 +220,22 @@ function Mypage() {
                                   </div>
                                 </div>
                                 <div className="btn-w">
-                                  <button className="btn-view" onClick={() => {navigate(`/campaign/detail/${matchedPost.id}`)}}>보러가기</button>
-                                  <button className="btn-view" onClick={() => {navigate(`/campaign/form/${matchedPost.id}/application`)}}>신청서 확인</button>
+                                  <button
+                                    className="btn-view"
+                                    onClick={() => {
+                                      navigate(`/campaign/detail/${matchedPost.id}`);
+                                    }}
+                                  >
+                                    보러가기
+                                  </button>
+                                  <button
+                                    className="btn-view"
+                                    onClick={() => {
+                                      navigate(`/campaign/form/${matchedPost.id}/application`);
+                                    }}
+                                  >
+                                    신청서 확인
+                                  </button>
                                 </div>
                               </div>
                             );
@@ -245,18 +257,18 @@ function Mypage() {
 
               {/* 탄소발자국 */}
               {activeTab === 2 && (
-                <div  className="mycont-wrap">
+                <div className="mycont-wrap">
                   <div className="cont-area">
-
-                    <UserMonthChart />
-                    {/* <div className="no-data-w">
+                    
+                      <UserMonthChart />
+                      {/* <div className="no-data-w">
                       <div className="no-data">
                         <p className="tit">발자국 계산 내역이 없습니다.</p>
                       </div>
                     </div> */}
-
-                  </div>
                     </div>
+                  </div>
+
               )}
             </div>
           </div>
